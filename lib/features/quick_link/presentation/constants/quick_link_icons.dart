cd2cef8c4b8747cb0ec49d93fbea3b5084cc390e
inshad-muhammed <inshad.muhammed@omnicoredigital.com>
2025-10-30 13:56:52 +0530

    Updated Quick link page

--- lib/features/quick_link/presentation/constants/quick_link_icons.dart
+++ lib/features/quick_link/presentation/constants/quick_link_icons.dart
@@ -3,20 +3,32 @@ import 'package:tasks/core/constants/app_strings/parts/quick_link_page.dart';
 
 final s = DefaultStrings.instance;
 final List<Map<String, dynamic>> labels = [
-  {'imagePath': "assets/quick_link_icons/eipo.svg", 'label': s.label1},
-  {'imagePath': "assets/quick_link_icons/transfers.svg", 'label': s.label2},
-  {'imagePath': "assets/quick_link_icons/stocks.svg", 'label': s.label3},
-  {'imagePath': "assets/quick_link_icons/investments.svg", 'label': s.label4},
-  {'imagePath': "assets/quick_link_icons/products.svg", 'label': s.label5},
-  {'imagePath': "assets/quick_link_icons/orders.svg", 'label': s.label6},
-  {'imagePath': "assets/quick_link_icons/requests.svg", 'label': s.label7},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
-  {'imagePath': "assets/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/eipo.svg", 'label': s.label1},
+  {
+    'imagePath': "assets/icons/quick_link_icons/transfers.svg",
+    'label': s.label2,
+  },
+  {'imagePath': "assets/icons/quick_link_icons/stocks.svg", 'label': s.label3},
+  {
+    'imagePath': "assets/icons/quick_link_icons/investments.svg",
+    'label': s.label4,
+  },
+  {
+    'imagePath': "assets/icons/quick_link_icons/products.svg",
+    'label': s.label5,
+  },
+  {'imagePath': "assets/icons/quick_link_icons/orders.svg", 'label': s.label6},
+  {
+    'imagePath': "assets/icons/quick_link_icons/requests.svg",
+    'label': s.label7,
+  },
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
+  {'imagePath': "assets/icons/quick_link_icons/default.svg", 'label': s.label8},
 ];
--- lib/features/quick_link/presentation/widgets/icon.dart
+++ lib/features/quick_link/presentation/widgets/icon.dart
@@ -13,6 +13,7 @@ class IconWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var showLimitMessage = false;
     final selectedIndexes = ref.watch(selectedIndexesProvider);
     final asyncQuickLinks = ref.watch(quickLinksProvider);
 
@@ -23,91 +24,106 @@ class IconWidget extends ConsumerWidget {
         child: ShimmerEffect(),
       ),
       error: (error, stack) => Center(child: Text("Error: $error")),
-      data: (labels) => Wrap(
-        spacing: SizeConfig.screenWidth * 0.03,
-        runSpacing: SizeConfig.screenWidth * 0.05,
-        children: List.generate(labels.length, (index) {
-          final item = labels[index];
-          final isSelected = selectedIndexes.contains(index);
+      data: (labels) => Column(
+        children: [
+          Wrap(
+            spacing: SizeConfig.screenWidth * 0.03,
+            runSpacing: SizeConfig.screenWidth * 0.05,
+            children: List.generate(labels.length, (index) {
+              final item = labels[index];
+              final isSelected = selectedIndexes.contains(index);
 
-          return GestureDetector(
-            onTap: () {
-              ref.read(selectedIndexesProvider.notifier).update((state) {
-                final newState = {...state};
-                if (newState.contains(index)) {
-                  newState.remove(index); // Unselect
-                } else {
-                  if (newState.length < 4) {
-                    newState.add(index); // Select only if < 4
-                  }
-                }
-                return newState;
-              });
-            },
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  width: SizeConfig.screenWidth * 0.15,
-                  height: SizeConfig.screenWidth * 0.15,
-                  child: AnimatedIconWidget(
-                    child: Stack(
-                      children: [
-                        Container(
-                          height: SizeConfig.screenWidth * 0.13,
-                          width: SizeConfig.screenWidth * 0.13,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(
-                              color: Colors.grey.withAlpha(100),
-                              width: 0.5,
+              return GestureDetector(
+                onTap: () {
+                  ref.read(selectedIndexesProvider.notifier).update((state) {
+                    final newState = {...state};
+                    if (newState.contains(index)) {
+                      newState.remove(index); // Unselect
+                    } else {
+                      if (newState.length < 4) {
+                        newState.add(index); // Select only if < 4
+                      } else {
+                        showLimitMessage = true;
+                      }
+                    }
+                    return newState;
+                  });
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      width: SizeConfig.screenWidth * 0.15,
+                      height: SizeConfig.screenWidth * 0.15,
+                      child: AnimatedIconWidget(
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: SizeConfig.screenWidth * 0.13,
+                              width: SizeConfig.screenWidth * 0.13,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.grey.withAlpha(100),
+                                  width: 0.5,
+                                ),
+                                borderRadius: BorderRadius.circular(
+                                  SizeConfig.screenWidth * 0.04,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.all(
+                                  SizeConfig.screenWidth * 0.026,
+                                ),
+                                child: SvgPicture.asset(
+                                  item.imagePath,
+                                  height: SizeConfig.screenWidth * 0.01,
+                                  width: SizeConfig.screenWidth * 0.01,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
                             ),
-                            borderRadius: BorderRadius.circular(
-                              SizeConfig.screenWidth * 0.04,
-                            ),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(
-                              SizeConfig.screenWidth * 0.026,
-                            ),
-                            child: SvgPicture.asset(
-                              item.imagePath,
-                              height: SizeConfig.screenWidth * 0.01,
-                              width: SizeConfig.screenWidth * 0.01,
-                              fit: BoxFit.contain,
-                            ),
-                          ),
+                            if (isSelected)
+                              Positioned(
+                                bottom: 5,
+                                right: 5,
+                                child: Icon(
+                                  Icons.check_circle,
+                                  color: Colors.green,
+                                  size: SizeConfig.screenWidth * 0.046,
+                                ),
+                              ),
+                          ],
                         ),
-                        if (isSelected)
-                          Positioned(
-                            bottom: 5,
-                            right: 5,
-                            child: Icon(
-                              Icons.check_circle,
-                              color: Colors.green,
-                              size: SizeConfig.screenWidth * 0.046,
-                            ),
-                          ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(height: SizeConfig.screenWidth * 0.02),
-                SizedBox(
-                  width: SizeConfig.screenWidth * 0.2,
-                  child: Text(
-                    item.label,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: SizeConfig.screenWidth * 0.028,
+                    SizedBox(height: SizeConfig.screenWidth * 0.02),
+                    SizedBox(
+                      width: SizeConfig.screenWidth * 0.2,
+                      child: Text(
+                        item.label,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: SizeConfig.screenWidth * 0.028,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        }),
+              );
+            }),
+          ),
+          showLimitMessage
+              ? Text(
+                  "Deselect a selected option to select a new one",
+                  style: TextStyle(
+                    color: Color(0xffC65851),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              : SizedBox(),
+        ],
       ),
     );
   }
