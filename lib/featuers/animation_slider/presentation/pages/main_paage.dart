5e9ce32de82896fdae7d0e7fd3c70b1a08a8b54f
inshad-muhammed <inshad.muhammed@omnicoredigital.com>
2025-11-19 18:50:09 +0530

    Updated

--- lib/featuers/animation_slider/presentation/pages/main_paage.dart
+++ lib/featuers/animation_slider/presentation/pages/main_paage.dart
@@ -4,10 +4,10 @@ class SlideUpAnimationPage extends StatefulWidget {
   const SlideUpAnimationPage({super.key});
 
   @override
-  State<SlideUpAnimationPage> createState() => _MoreBounceScreenState();
+  State<SlideUpAnimationPage> createState() => _SlideUpAnimationPageState();
 }
 
-class _MoreBounceScreenState extends State<SlideUpAnimationPage>
+class _SlideUpAnimationPageState extends State<SlideUpAnimationPage>
     with SingleTickerProviderStateMixin {
   bool isExpanded = false;
 
@@ -31,71 +31,64 @@ class _MoreBounceScreenState extends State<SlideUpAnimationPage>
 
       body: Stack(
         children: [
-          /// main content
           Positioned.fill(child: Center(child: Text("Main Screen Content"))),
-
-          /// Expanding panel with bounce effect
           AnimatedPositioned(
-            duration: Duration(milliseconds: 1500), // slower animation
-            curve: Curves.easeOutBack, // smoother soft bounce
+            duration: Duration(milliseconds: 1500),
+            curve: Curves.easeOutBack,
             bottom: 0,
             left: 0,
             right: 0,
             height: isExpanded ? height * 0.85 : 140,
-            child: Container(
-              decoration: BoxDecoration(color: Colors.transparent),
-              child: Column(
-                children: [
-                  GestureDetector(
-                    onTap: () => setState(() => isExpanded = !isExpanded),
-                    child: Container(
-                      height: 120,
-                      margin: EdgeInsets.symmetric(horizontal: 16),
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 15,
-                            offset: Offset(0, 6),
+            child: AnimatedOpacity(
+              opacity: 1,
+              duration: Duration(milliseconds: 1000),
+              curve: Curves.easeOutCubic,
+              child: IgnorePointer(
+                ignoring: !isExpanded,
+                child: ListView.separated(
+                  padding: EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+                  // Show main item when isExpanded is false
+                  // Show full listwhen isExpanded is true
+                  itemCount: isExpanded ? 21 : 1,
+                  separatorBuilder: (_, __) => SizedBox(height: 10),
+                  itemBuilder: (_, i) {
+                    if (i == 0) {
+                      //Main item
+                      return GestureDetector(
+                        onTap: () => setState(() => isExpanded = !isExpanded),
+                        child: Container(
+                          height: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 15,
+                                offset: Offset(0, 6),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Main Item",
-                        style: TextStyle(color: Colors.white, fontSize: 22),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 10),
-
-                  Expanded(
-                    child: AnimatedOpacity(
-                      opacity: isExpanded ? 1 : 0,
-                      duration: Duration(milliseconds: 1000), // slower fade
-                      curve: Curves.easeOutCubic, // smoother fade
-                      child: IgnorePointer(
-                        ignoring: !isExpanded,
-                        child: ListView.separated(
-                          padding: EdgeInsets.all(16),
-                          itemCount: 20,
-                          separatorBuilder: (_, __) => SizedBox(height: 10),
-                          itemBuilder: (_, i) => Container(
-                            padding: EdgeInsets.all(16),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Text("Item $i"),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Main Item",
+                            style: TextStyle(color: Colors.white, fontSize: 22),
                           ),
                         ),
+                      );
+                    }
+
+                    //other list items
+                    return Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    ),
-                  ),
-                ],
+                      child: Text("Item ${i - 1}"),
+                    );
+                  },
+                ),
               ),
             ),
           ),
